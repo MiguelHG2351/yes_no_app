ef77f37c5cd3332c7cc5f84f2d87f514ddeb54bf
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2023-12-21 11:45:36 -0600

    added option for send message

--- lib/presentacion/widgets/shared/message_field_box.dart
+++ lib/presentacion/widgets/shared/message_field_box.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
     final textController = TextEditingController();
     final focusNode = FocusNode();
-    
+
     final inputDecoration = InputDecoration(
-        hintText: 'Escribe un mensaje',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send),
-          onPressed: () {
-            final textValue = textController.value.text;
-            print('Botón: $textValue');
-            textController.clear();
-          },
-        ),
-      );
-    
+      hintText: 'Escribe un mensaje',
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send),
+        onPressed: () {
+          final textValue = textController.value.text;
+          textController.clear();
+          onValue(textValue);
+        },
+      ),
+    );
+
     return TextFormField(
       decoration: inputDecoration,
       onTapOutside: (event) {
@@ -31,11 +33,18 @@ class MessageFieldBox extends StatelessWidget {
       },
       focusNode: focusNode,
       onFieldSubmitted: (value) {
-        print('Submit value $value');
+        if (!value.isNotEmpty) {
+          ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Please, send a message with some text'),
+          ),
+        );
+        return;
+        }
+
         textController.clear();
-      },
-      onChanged: (value) {
-        print('Change value $value');
+        focusNode.requestFocus();
+        onValue(value);
       },
       controller: textController,
     );
