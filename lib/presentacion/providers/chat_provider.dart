f12391a89823755b1df0d4c9214dd1a3ac52bccb
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2023-12-26 07:11:03 -0600

    add chat_provider and response message when send an '?' in the end message

--- lib/presentacion/providers/chat_provider.dart
+++ lib/presentacion/providers/chat_provider.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helper/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
+  final getYesNoAnswer = GetYesNoAnswer();
+
   List<Message> messageList = [
     Message(text: 'Hola bro!', fromWho: FromWho.me),
     Message(text: 'Qué onda! 😎', fromWho: FromWho.me),
@@ -9,8 +14,32 @@ class ChatProvider extends ChangeNotifier {
     Message(text: 'Qué onda! 😎', fromWho: FromWho.me),
   ];
 
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
   Future<void> addMessage(String text) async {
+    if (text.isEmpty) return;
+
+    if (text.endsWith('?')) {
+      herReply();
+    }
+
     messageList.add(Message(text: text, fromWho: FromWho.me));
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeOut);
   }
 }
